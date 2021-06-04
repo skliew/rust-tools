194f8650ebe84fe38650939df08919af67e9fbea
skliew <skliew@gmail.com>
2021-06-04 10:23:21 +0800

    Add compact-json

---
+++ src/compact_json.rs
@@ -0,0 +1,10 @@
+extern crate serde_json;
+
+use serde_json::{Value};
+
+fn main() -> std::io::Result<()> {
+    let value: Value = serde_json::from_reader(std::io::stdin())?;
+    let pretty_string = serde_json::to_string(&value)?;
+    println!("{}", pretty_string);
+    Ok(())
+}
