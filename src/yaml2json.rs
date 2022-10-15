51a0a7716105bc3ff7361d1743372b72f43d7fbe
skliew <skliew@gmail.com>
2022-10-16 01:29:25 +0800

    Add yaml2json

---
+++ src/yaml2json.rs
@@ -0,0 +1,12 @@
+use serde_json;
+use serde_yaml;
+
+use std::result::Result;
+use serde_yaml::Value;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let value : Value = serde_yaml::from_reader(std::io::stdin())?;
+    serde_json::to_writer_pretty(std::io::stdout(), &value)?;
+    Ok(())
+}
